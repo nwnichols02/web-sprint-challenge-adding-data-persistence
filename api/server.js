a0e33bb2c09d0b256b6479459cb5d02e038e34d7
Nathan Nichols <nwnichols02@gmail.com>
2022-04-22 10:13:25 -0600

    npm dependancies installed, server up, tests 1/17 passing

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+// const projectrouter
+
+const server = express();
+
+server.use(express.json());
+// server.use('/api/project..., project router')
+
+server.use('/',  (req, res) => {
+    res.send('<h1>working yay!</h1>')
+})
+
+module.exports = server;
