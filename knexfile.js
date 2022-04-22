0e4d5d37cb41898a75e81171d4c5d608f8089edd
Nathan Nichols <nwnichols02@gmail.com>
2022-04-22 15:00:34 -0600

    meets MVP

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,7 @@
 const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
-  migrations: { directory: './data/migrations' }, disableMigrationsListValidation: true ,
-  disableTransactions: true,
+  migrations: { directory: './data/migrations' }, 
   pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
 }
 
